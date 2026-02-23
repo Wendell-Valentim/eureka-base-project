5abc1c166a715fe980675c7434408b95bbdb81b9
Wendell Valentim <wendelldasilva8@gmail.com>
2026-02-23 17:43:01 -0300

    feat: add cards microservice and gateway routes

--- mscloudgateway/src/main/java/io/github/wendellvalentim/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/wendellvalentim/mscloudgateway/MscloudgatewayApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,6 +15,11 @@ public class MscloudgatewayApplication {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
 	}
 
-	public RouteLocator
+	@Bean
+	public RouteLocator route(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+				.build();
+	}
 
 }
