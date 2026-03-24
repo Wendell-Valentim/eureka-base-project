11f415bc737bf5a54b3988a62e4584a1f1224138
Wendell-Valentim <wendellldasiilva8@gmail.com>
2026-03-24 10:03:00 -0300

    feat: complete card issuance flow via RabbitMQ

--- mscartoes/src/main/java/io/github/wendellvalentim/mscartoes/infra/mqueue/subscribre/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/io/github/wendellvalentim/mscartoes/infra/mqueue/subscribre/EmissaoCartaoSubscriber.java
@@ -1,6 +1,7 @@
 package io.github.wendellvalentim.mscartoes.infra.mqueue.subscribre;
 
 import io.github.wendellvalentim.mscartoes.infra.mqueue.records.DadosCartao;
+import io.github.wendellvalentim.mscartoes.model.Cartao;
 import io.github.wendellvalentim.mscartoes.model.ClienteCartao;
 import io.github.wendellvalentim.mscartoes.model.DadosSolicitacaoEmissaoCartao;
 import io.github.wendellvalentim.mscartoes.repository.CartaoRepository;
@@ -10,6 +11,8 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 public class EmissaoCartaoSubscriber {
@@ -18,6 +21,13 @@ public class EmissaoCartaoSubscriber {
 
     @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
     public void receberSolicitacaoEmissao(@Payload DadosSolicitacaoEmissaoCartao dados) {
-        var cartao =
+        Cartao cartao = cartaoRepository.findById(dados.idCartao()).orElseThrow();
+
+        var clienteCartao = new ClienteCartao();
+        clienteCartao.setCartao(cartao);
+        clienteCartao.setCpf(dados.cpf());
+        clienteCartao.setLimite(dados.limiteLiberado());
+
+        clienteCartaoRepository.save(clienteCartao);
     }
 }
